66dc881a4e9dd6e0a356cbc2a7b5de48ae5ef934
Marcos Melo <suiciniv2016+github@outlook.com>
2023-09-22 20:37:24 -0300

    build: :heavy_plus_sign: setup prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [import("prettier-plugin-tailwindcss")],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  quoteProps: "consistent",
+};
